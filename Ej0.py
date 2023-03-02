238cae21ccf97ace0d822e6e83cb46eb7e380c34
salinasromerodaniel <42517271+salinasromerodaniel@users.noreply.github.com>
2023-03-01 19:34:05 -0600

    Create Ej0.py

---
+++ Ej0.py
@@ -0,0 +1,8 @@
+n = int(input("¿Cuántos números deseas sumar? "))
+suma = 0.0
+
+for i in range(n):
+    num = float(input("Introduce el número {}: ".format(i+1)))
+    suma += num
+
+print("La suma total es: ", suma)
